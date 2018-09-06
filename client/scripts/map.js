e0ee6de84251741349a55c26c4f76a2df928cb4b
JamieRez <jamierezendes@gmail.com>
2018-09-05 19:28:59 -0700

    Updated for Parker

--- client/scripts/map.js
+++ client/scripts/map.js
@@ -1,8 +1,10 @@
 showMapWithUserLocation = (userLoc) => {
-  console.log(userLoc);
   var map = new google.maps.Map(document.getElementById('map'), {
     center: userLoc,
-    zoom: 8
+    zoom: 18,
+    styles : mapStyles,
+    disableDefaultUI: true,
+    zoomControl : true
   });
 
 }
